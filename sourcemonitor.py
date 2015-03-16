e78251802de38575155798fe2c1b09bf7ef1d42c
Bevan Christians <gaelicgod@gmail.com>
2015-03-16 07:53:51 +0200

    added templates and login system

--- sourcemonitor.py
+++ sourcemonitor.py
@@ -1,29 +1,59 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask.ext.pymongo import PyMongo
 
-projects = [
-    'sanlam_reality2',
-    'redbull-editions',
-    'mlp',
-    'zoo',
-]
+import json, urllib, datetime 
+
+SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+DB_NAME = 'source-control'
+
+
+app = Flask(__name__)
+app.secret_key = SECRET_KEY
+app.name = 'source-control'
+#app.config['MONGO_URI'] = 'mongodb://localhost:27017/'
+#app.config['MONGO_DBNAME'] = 'source-control'
+
+client = PyMongo(app)
+
+#users = client.db.users
+#projects = client.db.projects
+
 
 @app.route('/')
 def index():
-    return 'Index Page!'
+    if 'username' in session:
+        return render_template('source_list.html', username=session['username'])
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
-        do_the_login()
-    else:
-        show_the_login_form()
+        username = request.form['username']
+        password = request.form['password']
+        user = client.db.users.find_one({ "username": username })
 
-def do_the_login():
-    pass
+        if(user is not None):
+            if(user["password"] == password):
+                print(user)
+                session['username'] = user["username"]
+                session['email'] = user['email']
+                flash('Login successful')
+                return redirect(url_for('index'))
+            else:
+                error = "Invalid Password!"
+        else:
+            error = "Invalid Username!"
+    
+    return render_template('show_login.html', error=error)
+    
 
-def show_the_login_form():
-    pass
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
